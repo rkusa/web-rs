91b1c2a76eb59b3b63f6bbb6cfd93ac818ededb2
Markus Ast <m@rkusa.st>
2017-06-11 19:44:32 +0200

    add error-related traits to Error

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,6 @@
+use std::error::Error as StdError;
+use std::fmt;
+
 use hyper::server::Response;
 use hyper::status::StatusCode;
 
@@ -10,10 +13,26 @@ pub enum Error {
 impl Error {
     pub fn into_response(self) -> Response {
         match self {
-            Error::Status(status) => {
-                Response::new().with_status(status)
-            }
+            Error::Status(status) => Response::new().with_status(status),
             Error::Response(resp) => resp,
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::Status(ref status) => write!(f, "Error with status: {}", status),
+            Error::Response(ref resp) => write!(f, "Error with response:\n{:?}", resp),
+        }
+    }
+}
+
+impl StdError for Error {
+    fn description(&self) -> &str {
+        match *self {
+            Error::Status(_) => "Error with status code",
+            Error::Response(_) => "Error with response",
         }
     }
 }
